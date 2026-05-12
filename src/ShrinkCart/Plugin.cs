8705e743206c6f405dc5be84e8820fbd1bce924e
AngelcoMilk-天使棉 <2326412638@qq.com>
2026-05-13 02:08:23 +0800

    Release ShrinkCart v0.2.8

--- src/ShrinkCart/Plugin.cs
+++ src/ShrinkCart/Plugin.cs
@@ -11,7 +11,7 @@ namespace ShrinkCart
     {
         public const string PluginGuid = "AngelcoMilk.ShrinkCart";
         public const string PluginName = "ShrinkCart";
-        public const string PluginVersion = "0.2.7";
+        public const string PluginVersion = "0.2.8";
 
         internal static Plugin Instance;
         internal static ManualLogSource Log;
